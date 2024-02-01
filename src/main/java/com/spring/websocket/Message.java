bf1b2958d016bdd5f15f55bb9f05cdf45e7b2909
Digital <digital@Macbooks-MacBook-Pro.local>
2024-02-01 17:09:27 +0545

    Initial commit

---
+++ src/main/java/com/spring/websocket/Message.java
@@ -0,0 +1,16 @@
+package com.spring.websocket;
+
+public class Message {
+    private String text;
+    private String to;
+    public String getText() {
+        return text;
+    }
+    public String getTo() {
+        return to;
+    }
+    public void setTo(String to) {
+        this.to = to;
+    }
+    
+}
